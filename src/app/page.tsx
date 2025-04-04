524c6aa52887614eaf4fe71f6df5c9dd9e38ddb4
langadev <alfredolanga360@gmail.com>
2025-04-03 19:46:19 -1000

    ADD: other01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "@/components/Navbar";
 import Hero from '@/components/Hero';
 import Posts from '@/components/Posts';
-import Footer from '@/components/Footer';
+
 
 export default function page() {
   return (
