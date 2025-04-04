0098894a4ace5048000dc6032f53a61b74c6abb0
langadev <alfredolanga360@gmail.com>
2025-04-03 19:41:48 -1000

    ADD: other

--- src/app/contract/page.tsx
+++ src/app/contract/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { Calendar, Clock, DollarSign, FileText, Mail, User } from 'lucide-react';
+import {   DollarSign, FileText, User } from 'lucide-react';
 
 const ContratacaoFreelancer = () => {
   const [formData, setFormData] = useState({
